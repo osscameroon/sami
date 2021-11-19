bdd8fd9a031ec3f0efd5267b4fcce92a71d05d50
elhmn <nleme@live.fr>
2021-11-19 12:18:04 +0100

    feat: add docker-compose deployment scripts

---
+++ scripts/stop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USAGE="./deploy.sh <deploment_directory>
+Example: ./deploy.sh /Users/user_name/deployments/services
+The deploment_directory should not end with a \"/\""
+
+DEPLOY_DIR=$1
+
+if [ -z "$DEPLOY_DIR" ]; then
+	echo "Wrong usage: $USAGE"
+	exit 0
+fi
+
+SERVICE_DIR="services"
+CONF_DIR="conf"
+DEPLOY_SERVICE_DIR=$DEPLOY_DIR/$SERVICE_DIR
+DEPLOY_CONF_DIR=$DEPLOY_DIR/$CONF_DIR
+
+echo "DEPLOY_CONF_DIR: $DEPLOY_CONF_DIR"
+echo "DEPLOY_SERVICE_DIR: $DEPLOY_SERVICE_DIR"
+
+ls $DEPLOY_SERVICE_DIR
+ls $DEPLOY_CONF_DIR
+
+echo "Start deployment..."
+cd $DEPLOY_SERVICE_DIR
+DOCKER_COMPOSE_FILES="$(find . -name "docker-compose.*")"
+echo "DOCKER_COMPOSE_FILES:"
+echo "$DOCKER_COMPOSE_FILES"
+
+for file in $DOCKER_COMPOSE_FILES; do
+	set -x
+	docker-compose -f "$file" down
+done
